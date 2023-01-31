54569ec1a934b1ce74940da3b5c748533c00d75f
cristamer <122634530+cristamer@users.noreply.github.com>
2023-01-31 11:47:56 -0500

    valida user 2

--- programar/valida/valida_user.php
+++ programar/valida/valida_user.php
@@ -7,7 +7,7 @@ if (isset($_POST['usuario'])) {
 	$dni = $_POST['usuario'];
 	$clave = $_POST['pass'];
 
-$query="SELECT * FROM usuarios WHERE user_dni= '$dni' and user_clave= '$clave'";
+$query="SELECT * FROM usuarios WHERE user_dni= '$dni' and user_clave= '$clave' and user_perfil <> 2 ";
 	$result=mysqli_query($conexion, $query);
 	$numfilas = mysqli_num_rows($result);
 	$filas=mysqli_fetch_assoc($result);
